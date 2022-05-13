6ce8122b9580f2f7e009a9e5620f430a28c25dd5
Your Name <hello@kevincadena.com>
2022-05-12 22:52:01 -0400

    working on the footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,6 +3,7 @@
         <!-- TODO: Speak with XXKM about what should go in the footer? 
         Copyright / Social / Etc
         -->
+        <p> License Information or Social Media or Something Else </p>
         </footer>
         <?= js([
         'assets/js/index.js',
