e9d384ceb279ad168b9f2db998fb2e1275398842
Your Name <hello@kevincadena.com>
2022-06-03 14:46:25 -0400

    finalizing other CSS pages

--- site/templates/downloads.php
+++ site/templates/downloads.php
@@ -1,9 +1,16 @@
 <?php snippet('header') ?>
 <h1> <?= $page->title() ?></h1> 
-<section>
+<?php if($page->downloadText()): ?>
+    <section id="download-text">
+        <?= $page->downloadText()->kt() ?>
+    </section>
+<?php endif ?>
+<section id="downloads">
 <?php $downloads = $page->download()->toStructure();
 foreach($downloads as $download): ?>
+    <h3><?= $download->title()?></h3>
     <?= $download->description()->kt() ?>
-    <a class="download" href="<?= $download->file()->toFile() ?>"><?= $download->title() ?> </a>
+    <a class="download button" href="<?= $download->file()->toFile() ?>">Download <?= $download->title() ?> </a>
 <?php endforeach ?> 
+</section>
 <?php snippet('footer')?>
--- site/templates/resources.php
+++ site/templates/resources.php
@@ -1,10 +1,15 @@
 <?php snippet('header') ?>
 <h1><?= $page->title() ?></h1>
+<?php if($page->resourcesText()): ?>
+<section id="resources-text">
+    <?= $page->resourcesText()->kt() ?>
+</section>
+<?php endif ?>
 <section class="resources-grid">
 <?php $items = $page->resources()->toStructure();
 foreach ($items as $item): ?>
     <a class="link" href="<?= $item->url()?>">    
-        <?= $item->image()->toFile() ?>
+        <figure class="img" style="--w: 16; --h:9"><?= $item->image()->toFile() ?></figure>
         <h5> <?= $item->title() ?></h5>     
         <?= $item->description()->kt() ?>
         </a>
