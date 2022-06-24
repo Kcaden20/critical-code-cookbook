f6867d9e13dfb33d465e7c773b61d30a824c5c46
Your Name <hello@kevincadena.com>
2022-06-24 15:21:44 -0400

    latest changes

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,7 +12,6 @@
             'assets/css/fonts.css', 
             'assets/css/var.css',
             'assets/css/index.css',
-            'assets/css/dark-theme.css',
             'assets/css/media.css',
             'assets/css/lightbox.css',
             'assets/css/background.css',
@@ -51,12 +50,16 @@
             <?php if($page->template() == 'contribution'):?>
                 <nav class="subnav">
                     <a href="#title"> Introduction </a>
-                    <a href="#body">Body</a>
+                    <a href="#body">The Recipe</a>
                     <?php
                     if($page->template() == 'contribution') {$contributions = $page->children();}
                     foreach($contributions as $contribution): ?>
                         <a href="#<?= $contribution->uid()?>"> <?= $contribution->title() ?></a>
                     <?php endforeach ?>
+                    <?php if($page->context() == 'true'):?>
+                        <a href="#context">Q&A</a>
+                    </a>
+                    <?php endif ?>
                 </nav>
             <?php endif ?>
         </header>
